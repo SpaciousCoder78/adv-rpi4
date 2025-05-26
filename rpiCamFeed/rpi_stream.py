8b02921fb6ef30734849ce4f70813c26cd619799
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2025-05-26 17:10:52 +0530

    Update rpi_stream.py

--- rpiCamFeed/rpi_stream.py
+++ rpiCamFeed/rpi_stream.py
@@ -1,26 +1,25 @@
 from flask import Flask, Response
 import cv2
+from picamera2 import Picamera2
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)
-#camera.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-#camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-#camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+picam = Picamera2()
+preview_config = picam.create_preview_configuration(main={"size": (640, 480)})  # adjust resolution as needed
+picam.configure(preview_config)
+picam.start()
+
 def generate_frames():
-	while True:
-		success,frame = camera.read()
-		if not success:
-			break
-		else:
-			ret, buffer = cv2.imencode('.jpg', frame)
-			frame = buffer.tobytes()
-			yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    while True:
+        frame = picam.capture_array()
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
-
 def video_feed():
-	return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000,debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
