d254f7db639f39eb63f549dab7090ce38a8fd4e5
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2025-06-17 12:47:58 +0530

    Update yolotest.py

--- yolotest/yolotest.py
+++ yolotest/yolotest.py
@@ -7,63 +7,64 @@ from yolov8 import YOLOv8
 # Global variables to share state
 detected_class = None
 detection_triggered = False
+# Motor A pins (Motor 1)
+in1 = 24
+in2 = 23
+ena = 25
+
+# Motor B pins (Motor 2)
+in3 = 17
+in4 = 22
+enb = 27
+
+temp1 = 1  # 1 for forward
+
+GPIO.setmode(GPIO.BCM)
+
+# Setup Motor A
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(ena, GPIO.OUT)
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+pA = GPIO.PWM(ena, 1000)
+pA.start(20)
+
+# Setup Motor B
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+GPIO.setup(enb, GPIO.OUT)
+GPIO.output(in3, GPIO.LOW)
+GPIO.output(in4, GPIO.LOW)
+pB = GPIO.PWM(enb, 1000)
+pB.start(20)
 
 def controlspeed():
     global detected_class, detection_triggered
 
-    # Motor A pins (Motor 1)
-    in1 = 24
-    in2 = 23
-    ena = 25
-
-    # Motor B pins (Motor 2)
-    in3 = 17
-    in4 = 22
-    enb = 27
-
-    temp1 = 1  # 1 for forward
-
-    GPIO.setmode(GPIO.BCM)
-
-    # Setup Motor A
-    GPIO.setup(in1, GPIO.OUT)
-    GPIO.setup(in2, GPIO.OUT)
-    GPIO.setup(ena, GPIO.OUT)
-    GPIO.output(in1, GPIO.LOW)
+    # Forward for both motors
+    GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
-    pA = GPIO.PWM(ena, 1000)
-    pA.start(20)
-
-    # Setup Motor B
-    GPIO.setup(in3, GPIO.OUT)
-    GPIO.setup(in4, GPIO.OUT)
-    GPIO.setup(enb, GPIO.OUT)
-    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in3, GPIO.HIGH)
     GPIO.output(in4, GPIO.LOW)
-    pB = GPIO.PWM(enb, 1000)
-    pB.start(20)
+    print("forward")
+    pA.ChangeDutyCycle(20)
+    pB.ChangeDutyCycle(20)
+    sleep(1)
 
     count = 1
     while True:
         print("Motor thread running")
         if temp1 == 1:
-            # Forward for both motors
-            GPIO.output(in1, GPIO.HIGH)
-            GPIO.output(in2, GPIO.LOW)
-            GPIO.output(in3, GPIO.HIGH)
-            GPIO.output(in4, GPIO.LOW)
-            print("forward")
-            pA.ChangeDutyCycle(50)
-            pB.ChangeDutyCycle(50)
-            sleep
+            
 
             # Check if detected class is 29 to perform stopping actions
-            if detected_class == 29:
+            if detected_class == 11:
                 print("Detected 70 Speed Sign")
                 detection_triggered = True
 
-                pA.ChangeDutyCycle(70)
-                pB.ChangeDutyCycle(70)
+                pA.ChangeDutyCycle(100)
+                pB.ChangeDutyCycle(100)
                 
                 
             if detected_class == 30:
@@ -72,6 +73,14 @@ def controlspeed():
                 
                 pA.ChangeDutyCycle(20)
                 pB.ChangeDutyCycle(20)
+            
+            if detected_class == 14:
+                print("Detected Stop Sign")
+                detection_triggered=True
+                pA.ChangeDutyCycle(0)
+                pB.ChangeDutyCycle(0)
+                
+    GPIO.cleanup()
                 
 
 outer_run = True
